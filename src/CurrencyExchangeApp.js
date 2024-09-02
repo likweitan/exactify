8a91898cefb71b3f61ce83a3e6a2ee3cc9cd0aee
likweitan <likweitan@gmail.com>
2024-09-02 14:07:36 +0800

    Added export functionality

--- src/CurrencyExchangeApp.js
+++ src/CurrencyExchangeApp.js
@@ -1,14 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import * as XLSX from 'xlsx';
-import Form from 'react-bootstrap/Form';
-import FormSelect from 'react-bootstrap/FormSelect';
+import React, { useState, useEffect } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import * as XLSX from "xlsx";
+import Form from "react-bootstrap/Form";
+import FormSelect from "react-bootstrap/FormSelect";
+import Button from "react-bootstrap/Button";
 
 const calculateMedian = (data) => {
-  const rates = data.map(item => item.CIMBRate).filter(rate => rate !== '-');
+  const rates = data
+    .map((item) => item.CIMBRate)
+    .filter((rate) => rate !== "-");
   const sortedRates = rates.sort((a, b) => a - b);
   const middle = Math.floor(sortedRates.length / 2);
 
@@ -22,7 +34,9 @@ const calculateMedian = (data) => {
 const getYAxisDomain = (data) => {
   const median = calculateMedian(data);
   const range = 0.05; // Range around the median (5% in this case)
-  const rates = data.map(item => item.CIMBRate).filter(rate => rate !== '-');
+  const rates = data
+    .map((item) => item.CIMBRate)
+    .filter((rate) => rate !== "-");
   const maxRate = Math.max(...rates);
   const minRate = Math.min(...rates);
 
@@ -34,7 +48,7 @@ const getYAxisDomain = (data) => {
 
 const CurrencyExchangeApp = () => {
   const [data, setData] = useState([]);
-  const [timeFrame, setTimeFrame] = useState('day');
+  const [timeFrame, setTimeFrame] = useState("day");
   const [chartData, setChartData] = useState([]);
   const [tableData, setTableData] = useState([]);
   const [latestRate, setLatestRate] = useState(null);
@@ -43,14 +57,18 @@ const CurrencyExchangeApp = () => {
   const yAxisDomain = getYAxisDomain(chartData);
 
   useEffect(() => {
-    fetch('https://raw.githubusercontent.com/likweitan/CIMB-exchange-rates/main/exchange_rates.json')
-      .then(response => response.json())
-      .then(jsonData => {
-        const formattedData = jsonData.map(item => ({
-          timestamp: new Date(item.timestamp),
-          rate: parseFloat(item.exchange_rate),
-          platform: item.platform
-        })).sort((a, b) => b.timestamp - a.timestamp); // Sort by timestamp descending
+    fetch(
+      "https://raw.githubusercontent.com/likweitan/CIMB-exchange-rates/main/exchange_rates.json"
+    )
+      .then((response) => response.json())
+      .then((jsonData) => {
+        const formattedData = jsonData
+          .map((item) => ({
+            timestamp: new Date(item.timestamp),
+            rate: parseFloat(item.exchange_rate),
+            platform: item.platform,
+          }))
+          .sort((a, b) => b.timestamp - a.timestamp); // Sort by timestamp descending
         setData(formattedData.reverse());
         setLatestRate(formattedData.at(-1)); // Set the latest rate
       });
@@ -59,20 +77,29 @@ const CurrencyExchangeApp = () => {
   useEffect(() => {
     const processData = () => {
       let groupedData = {};
-      data.forEach(item => {
+      data.forEach((item) => {
         const date = item.timestamp;
         let key;
         switch (timeFrame) {
-          case 'hour':
-            key = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()).getTime();
+          case "hour":
+            key = new Date(
+              date.getFullYear(),
+              date.getMonth(),
+              date.getDate(),
+              date.getHours()
+            ).getTime();
             break;
-          case 'day':
-            key = new Date(date.getFullYear(), date.getMonth(), date.getDate()).getTime();
+          case "day":
+            key = new Date(
+              date.getFullYear(),
+              date.getMonth(),
+              date.getDate()
+            ).getTime();
             break;
-          case 'month':
+          case "month":
             key = new Date(date.getFullYear(), date.getMonth()).getTime();
             break;
-          case 'year':
+          case "year":
             key = new Date(date.getFullYear(), 0).getTime();
             break;
           default:
@@ -83,13 +110,13 @@ const CurrencyExchangeApp = () => {
         }
         groupedData[key][item.platform] = item.rate;
       });
-    
-      const processed = Object.keys(groupedData).map(key => ({
+
+      const processed = Object.keys(groupedData).map((key) => ({
         date: formatDate(new Date(Number(key))),
-        CIMBRate: groupedData[key]['CIMB'] || '-',
-        WISERate: groupedData[key]['WISE'] || '-',
+        CIMBRate: groupedData[key]["CIMB"] || "-",
+        WISERate: groupedData[key]["WISE"] || "-",
       }));
-    
+
       setChartData(processed); // Keep the chart data in ascending order
       setTableData([...processed].reverse()); // Reverse the data for the table
     };
@@ -99,19 +126,22 @@ const CurrencyExchangeApp = () => {
 
   const formatDate = (date) => {
     switch (timeFrame) {
-      case 'hour':
+      case "hour":
         return date.toLocaleString(undefined, {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit'
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
         });
-      case 'day':
+      case "day":
         return date.toLocaleDateString();
-      case 'month':
-        return date.toLocaleDateString(undefined, { year: 'numeric', month: 'long' });
-      case 'year':
+      case "month":
+        return date.toLocaleDateString(undefined, {
+          year: "numeric",
+          month: "long",
+        });
+      case "year":
         return date.getFullYear().toString();
       default:
         return date.toLocaleDateString();
@@ -121,10 +151,26 @@ const CurrencyExchangeApp = () => {
   const customTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
       return (
-        <div style={{ backgroundColor: 'white', padding: '10px', border: '1px solid #ccc' }}>
+        <div
+          style={{
+            backgroundColor: "white",
+            padding: "10px",
+            border: "1px solid #ccc",
+          }}
+        >
           <p>{formatDate(new Date(payload[0].payload.date))}</p>
-          <p>CIMB Rate: {payload[0].payload.CIMBRate !== '-' ? payload[0].payload.CIMBRate.toFixed(4) : '-'}</p>
-          <p>WISE Rate: {payload[0].payload.WISERate !== '-' ? payload[0].payload.WISERate.toFixed(4) : '-'}</p>
+          <p>
+            CIMB Rate:{" "}
+            {payload[0].payload.CIMBRate !== "-"
+              ? payload[0].payload.CIMBRate.toFixed(4)
+              : "-"}
+          </p>
+          <p>
+            WISE Rate:{" "}
+            {payload[0].payload.WISERate !== "-"
+              ? payload[0].payload.WISERate.toFixed(4)
+              : "-"}
+          </p>
         </div>
       );
     }
@@ -134,8 +180,8 @@ const CurrencyExchangeApp = () => {
   const exportToExcel = () => {
     const worksheet = XLSX.utils.json_to_sheet(tableData);
     const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Exchange Rates');
-    XLSX.writeFile(workbook, 'exchange_rates.xlsx');
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Exchange Rates");
+    XLSX.writeFile(workbook, "exchange_rates.xlsx");
   };
 
   // Pagination calculations
@@ -150,55 +196,126 @@ const CurrencyExchangeApp = () => {
 
   return (
     <div>
-      <div style={{ padding: '20px', maxWidth: '1200px', margin: '0 auto' }}>
-        <h1 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>SGD - MYR Exchange Rate</h1>
-
-        {latestRate && (
-          <div style={{
-            backgroundColor: '#f0f0f0',
-            padding: '20px',
-            borderRadius: '8px',
-            marginBottom: '20px',
-            boxShadow: '0 2px 4px rgba(0,0,0,0.1)'
-          }}>
-            <h2 style={{ fontSize: '18px', marginBottom: '10px' }}>Latest Exchange Rate</h2>
-            <p style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '5px' }}>
-              1 SGD = {latestRate.rate.toFixed(4)} MYR
-            </p>
-            <p style={{ fontSize: '14px', color: '#666' }}>
-              Last updated: {new Date(latestRate.timestamp).toLocaleString()}
-            </p>
-          </div>
-        )}
-
-        <div style={{ marginBottom: '20px' }}>
-          
-        </div>
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h1 class="h2">Dashboard</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-              {/* <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary" onClick={exportToExcel}>Export to Excel</button>
-              </div> */}
-              <div class="btn-group mr-2">
-          <Form.Select class="form-select"
-            id="timeframe-select"
-            value={timeFrame}
-            onChange={(e) => setTimeFrame(e.target.value)}
+      <div style={{ padding: "20px", maxWidth: "1200px", margin: "0 auto" }}>
+        <div>
+          <h1
+            style={{
+              fontSize: "24px",
+              fontWeight: "bold",
+              marginBottom: "20px",
+            }}
           >
-            <option value="hour">Hourly</option>
-            <option value="day">Daily</option>
-            <option value="month">Monthly</option>
-            <option value="year">Yearly</option>
-          </Form.Select>
-            </div>
+            SGD - MYR Exchange Rate
+          </h1>
+
+          <Container>
+            <Row>
+              <Col xs={12} md={6} lg={6}>
+                {latestRate && (
+                  <div
+                    style={{
+                      backgroundColor: "#EECAD5",
+                      padding: "20px",
+                      borderRadius: "8px",
+                      marginBottom: "20px",
+                      boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+                    }}
+                  >
+                    <h2 style={{ fontSize: "18px", marginBottom: "10px" }}>
+                      CIMB Latest Exchange Rate
+                    </h2>
+                    <p
+                      style={{
+                        fontSize: "24px",
+                        fontWeight: "bold",
+                        marginBottom: "5px",
+                      }}
+                    >
+                      1 SGD = {latestRate.rate.toFixed(4)} MYR
+                    </p>
+                    <p style={{ fontSize: "14px", color: "#666" }}>
+                      Last updated:{" "}
+                      {new Date(latestRate.timestamp).toLocaleString()}
+                    </p>
+                  </div>
+                )}
+              </Col>
+              <Col xs={12} md={6} lg={6}>
+                {latestRate && (
+                  <div
+                    style={{
+                      backgroundColor: "#F6EACB",
+                      padding: "20px",
+                      borderRadius: "8px",
+                      marginBottom: "20px",
+                      boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+                    }}
+                  >
+                    <h2 style={{ fontSize: "18px", marginBottom: "10px" }}>
+                      WISE Latest Exchange Rate
+                    </h2>
+                    <p
+                      style={{
+                        fontSize: "24px",
+                        fontWeight: "bold",
+                        marginBottom: "5px",
+                      }}
+                    >
+                      1 SGD = {latestRate.rate.toFixed(4)} MYR
+                    </p>
+                    <p style={{ fontSize: "14px", color: "#666" }}>
+                      Last updated:{" "}
+                      {new Date(latestRate.timestamp).toLocaleString()}
+                    </p>
+                  </div>
+                )}
+              </Col>
+            </Row>
+            {/* <Row>
+            <p style={{ fontSize: "14px", color: "#666", textAlign: 'center' }}>
+                      Last updated:{" "}
+                      {new Date(latestRate.timestamp).toLocaleString()}
+                    </p>
+            </Row> */}
+          </Container>
+        </div>
+
+        <div style={{ marginBottom: "20px" }}></div>
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+          <h1 class="h2">Dashboard</h1>
+          <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group mr-2">
+            <Button class="btn btn-sm btn-outline-secondary" style={{ marginRight: 10 }} onClick={exportToExcel}>Export</Button>
+                
+              </div>
+            <div class="btn-group mr-1">
+              <Form.Select
+                class="form-select btn-outline-secondary"
+                id="timeframe-select"
+                value={timeFrame}
+                onChange={(e) => setTimeFrame(e.target.value)}
+              >
+                <option value="hour">Hourly</option>
+                <option value="day">Daily</option>
+                <option value="month">Monthly</option>
+                <option value="year">Yearly</option>
+              </Form.Select>
             </div>
           </div>
+        </div>
         <Container>
           <Row>
-          <Col xs={12} md={6} lg={6}>
-              <div style={{ marginBottom: '40px' }}>
-                <h2 style={{ fontSize: '20px', fontWeight: 'semibold', marginBottom: '10px' }}>Exchange Rate Chart</h2>
+            <Col xs={12} md={6} lg={6}>
+              <div style={{ marginBottom: "40px" }}>
+                <h2
+                  style={{
+                    fontSize: "20px",
+                    fontWeight: "semibold",
+                    marginBottom: "10px",
+                  }}
+                >
+                  Exchange Rate Chart
+                </h2>
                 <ResponsiveContainer width="100%" height={400}>
                   <LineChart data={chartData}>
                     <CartesianGrid strokeDasharray="3 3" />
@@ -213,22 +330,41 @@ const CurrencyExchangeApp = () => {
                     <YAxis domain={yAxisDomain} />
                     <Tooltip content={customTooltip} />
                     <Legend />
-                    <Line type="monotone" dataKey="CIMBRate" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line
+                      type="monotone"
+                      dataKey="CIMBRate"
+                      stroke="#8884d8"
+                      activeDot={{ r: 8 }}
+                    />
                     <Line type="monotone" dataKey="WISERate" stroke="#82ca9d" />
                   </LineChart>
                 </ResponsiveContainer>
               </div>
             </Col>
             <Col xs={12} md={6} lg={6}>
-              <div style={{ marginBottom: '20px' }}>
-                <h2 style={{ fontSize: '20px', fontWeight: 'semibold', marginBottom: '10px' }}>Exchange Rate Data</h2>
-                <div style={{ height: '400px', overflowY: 'auto', marginBottom: '20px' }}>
+              <div style={{ marginBottom: "20px" }}>
+                <h2
+                  style={{
+                    fontSize: "20px",
+                    fontWeight: "semibold",
+                    marginBottom: "10px",
+                  }}
+                >
+                  Exchange Rate Data
+                </h2>
+                <div
+                  style={{
+                    height: "400px",
+                    overflowY: "auto",
+                    marginBottom: "20px",
+                  }}
+                >
                   <table className="table table-striped">
                     <thead>
                       <tr>
                         <th>Date</th>
-                        <th>CIMB Rate</th>
-                        <th>WISE Rate</th>
+                        <th>CIMB</th>
+                        <th>WISE</th>
                       </tr>
                     </thead>
                     <tbody>
@@ -241,27 +377,29 @@ const CurrencyExchangeApp = () => {
                       ))}
                     </tbody>
                   </table>
-                </div>
-                <div className="pagination">
+                  <div className="pagination">
                   {[...Array(totalPages)].map((_, index) => (
                     <button
                       key={index}
                       onClick={() => handlePageChange(index + 1)}
                       disabled={currentPage === index + 1}
                       style={{
-                        padding: '5px 10px',
-                        margin: '0 5px',
-                        backgroundColor: currentPage === index + 1 ? '#007bff' : '#ccc',
-                        color: '#fff',
-                        border: 'none',
-                        borderRadius: '5px',
-                        cursor: 'pointer'
+                        padding: "5px 10px",
+                        margin: "0 5px",
+                        backgroundColor:
+                          currentPage === index + 1 ? "#007bff" : "#ccc",
+                        color: "#fff",
+                        border: "none",
+                        borderRadius: "5px",
+                        cursor: "pointer",
                       }}
                     >
                       {index + 1}
                     </button>
                   ))}
                 </div>
+                </div>
+                
               </div>
             </Col>
           </Row>
