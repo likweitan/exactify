5750168b2624e225ff38e79d4c8eb1bcd1403e60
likweitan <likweitan@gmail.com>
2024-09-04 00:17:42 +0800

    Update logo

--- src/NavBar.js
+++ src/NavBar.js
@@ -29,7 +29,13 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
      <Container>
-      <Navbar.Brand as={Link} to="/">Reach</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/"><img
+              alt=""
+              src="/logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
