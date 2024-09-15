089c9337b29fc8bbbde64e36a7285c1d0c21e2cd
likweitan <likweitan@gmail.com>
2024-09-15 20:54:14 +0800

    Update home design

--- src/Home.js
+++ src/Home.js
@@ -2,64 +2,53 @@ import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+const AppIcon = ({ to, icon, title, subtitle, bgColor }) => (
+  <div className="app-icon mb-4" style={{ width: "75px" }}>
+    <Link to={to} className="text-decoration-none">
+      <div
+        className="d-flex justify-content-center align-items-center rounded"
+        style={{
+          width: "75px",
+          height: "75px",
+          backgroundColor: bgColor,
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <img src={icon} alt={title} width="52" height="52" />
+      </div>
+    </Link>
+    <div className="text-center mt-2">
+      <h6 className="mb-2">{title}</h6>
+      {/* <small className="text-muted">{subtitle}</small> */}
+    </div>
+  </div>
+);
+
 const Home = () => {
   return (
     <Container>
       <div
-        className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom"
+        className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2"
         style={{ marginTop: "1rem" }}
-      >
-        <h1 className="h3">Exactify</h1>
-        <div className="btn-toolbar mb-2 mb-md-0">
-          <div className="btn-group mr-1"></div>
-        </div>
-      </div>
-      {/* <Row>
-        <Col>
-          <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-dark rounded box-shadow">
-            <div class="lh-100">
-              <h6 class="mb-0 text-white lh-100">Welcome to Reach</h6>
-              <small>Since 2024</small>
-            </div>
-          </div>
-          </Col>
-        </Row> */}
-      <Row>
-        <Col xs={12} md={3}>
-          <Link to="/exchange" className="text-decoration-none">
-            <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-primary rounded box-shadow">
-              <img
-                class="mr-3"
-                src="https://images.vexels.com/media/users/3/146882/isolated/lists/7525685ed67fa782b7d851273e1264c7-currency-exchange.png"
-                alt=""
-                width="48"
-                height="48"
-                style={{ marginRight: "1rem" }}
-              />
-              <div class="lh-100">
-                <h6 class="mb-0 text-white lh-100">Exchange Rates</h6>
-                <small>SGD-MYR</small>
-              </div>
-            </div>
-          </Link>
+      ></div>
+      <Row className="justify-content-start">
+        <Col xs={3} sm={4} md={2} lg={1}>
+          <AppIcon
+            to="/exchange"
+            icon="https://images.vexels.com/media/users/3/146882/isolated/lists/7525685ed67fa782b7d851273e1264c7-currency-exchange.png"
+            title="Exchange Rates"
+            subtitle="SGD-MYR"
+            bgColor="#F5F7F8"
+          />
         </Col>
-        <Col xs={12} md={3}>
-          <Link to="/loancalculator" className="text-decoration-none">
-            <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-success rounded box-shadow">
-              <img
-                class="mr-3"
-                src="https://ps.w.org/loan-calculator-wp/assets/icon-256x256.png?rev=2611572"
-                alt=""
-                width="48"
-                height="48"
-                style={{ marginRight: "1rem" }}
-              />
-              <div class="lh-100">
-                <h6 class="mb-0 text-white lh-100">Loan Calculator</h6>
-                <small>Housing</small>
-              </div>
-            </div>
-          </Link>
+        <Col xs={3} sm={4} md={2} lg={1}>
+          <AppIcon
+            to="/loancalculator"
+            icon="https://ps.w.org/loan-calculator-wp/assets/icon-256x256.png?rev=2611572"
+            title="Loan Calculator"
+            subtitle="Housing"
+            bgColor="#F5F7F8"
+          />
         </Col>
       </Row>
     </Container>
