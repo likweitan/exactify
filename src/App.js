550344bacd0be2644ee8016b48980218b653cd3e
likweitan <likweitan@gmail.com>
2024-10-01 18:09:25 +0800

    Added Vercel Speed Insights

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from "react";
 import CurrencyExchangeApp from "./CurrencyExchangeApp";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <div className="App">
       <CurrencyExchangeApp />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
