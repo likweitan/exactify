262c59856d212fbc4a6c897bab6e1ac3d73cff6c
likweitan <likweitan@gmail.com>
2024-10-01 18:08:39 +0800

    Added Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // App.js remains the same
-import React from 'react';
-import CurrencyExchangeApp from './CurrencyExchangeApp';
+import React from "react";
+import CurrencyExchangeApp from "./CurrencyExchangeApp";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <CurrencyExchangeApp />
+      <Analytics />
     </div>
   );
 }
